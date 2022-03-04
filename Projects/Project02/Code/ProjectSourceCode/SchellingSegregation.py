314271f8aff111136d312f9a5822ae63634dd4dc
Robert Denim Horton <Denim_horton@hotmail.com>
2022-03-03 19:15:18 -0700

    Adding in stuff berfore i delete it all

--- Projects/Project02/Code/ProjectSourceCode/SchellingSegregation.py
+++ Projects/Project02/Code/ProjectSourceCode/SchellingSegregation.py
@@ -4,23 +4,31 @@ from black import format_file_contents
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
-import math, random, time, logging, json, glob, os
+import math, random, time, logging, json, glob, os, sys, shutil
 from matplotlib.animation import FuncAnimation
 from alive_progress import alive_bar
+from PIL import Image
+
  
+'''
+File/Program Configuration Setup
+'''
 logging.basicConfig(filename='Content\\LogInfo\\logInfo.log', filemode='w', level=logging.INFO)
 logging = logging.getLogger('alive_progress')
 
 team_map = {0:"Empty Space", 1:"Red Residence", -1:"Blue Residence"}
 
-files = glob.glob('./Content/**/*.png', recursive=True)
+files = glob.glob('.\\Content\\**\\**\\*.png', recursive=True)
+print(type(files))
+files.append(glob.glob('.\\Content\\**\\**\\'))
+print(files)
 
-
-for f in files:
+for i in list(files[0]):
     try:
-        os.remove(f)
+        shutil.rmtree(i)
     except OSError as e:
-        print("Error: %s : %s" % (f, e.strerror))
+        print("Error: %s : %s" % (i, e.strerror))
+
 
 class GraphGenerator(object):
     def __init__(self, t=0.5, os=0.5, mxd=0.5, g_size=20, grph_ind_num="TestDefault", num_iter = 100):
@@ -44,16 +52,32 @@ class GraphGenerator(object):
         obj_str += f"Graph:\n{self.graph}"
         return obj_str 
 
+    def setTValue(self, t_v):
+        self.t_value=t_v
+
+    def setOSValue(self, o_spts):
+        self.open_spots=o_spts
+
+    def setMixedValue(self, mxd):
+        self.t_value=mxd
+
+
     def showGraph(self, iter=0):
         plt.matshow(self.graph, cmap='seismic')
         plt.title(f"{self.graph_instance_name}-{iter:03}")
         plt.show()
         plt.close()
 
-    def saveGraph(self, iter=0):
+    def saveGraph(self, iter=0, epsd=0):
+        sub_dir=f"{epsd}"
         plt.matshow(self.graph, cmap='seismic')
         plt.title(f"{self.graph_instance_name}-{iter:03}")
-        plt.savefig(f".\\Content\\GenoratedGraphs\\{self.graph_instance_name}-{iter:03}.png")
+        save_img_path = f".\\Content\\GenoratedGraphs\\GraphTestSet{sub_dir:03}"
+        isExist = os.path.exists(save_img_path)
+        if not isExist:
+            os.makedirs(save_img_path)
+
+        plt.savefig(f"{save_img_path}\\{self.graph_instance_name}-{iter:03}.png")
         plt.close()
 
 
@@ -150,12 +174,12 @@ class GraphGenerator(object):
                 if graph_ctf >= 1.0:
                     logging.info(f"Homophily! Segregation achived: {graph_ctf}")
                     print(f"\t{self.graph_instance_name}: {graph_ctf}")
-                    self.saveGraph(iter)
+                    self.saveGraph(iter, epsd=self.graph_instance_name)
                     break
                 if iter % (self.num_of_iterations / 5) == 0:
                     print(f"\t{self.graph_instance_name}: {graph_ctf}")
                     logging.info(f"\tIteration {iter:>3} : CTF : {graph_ctf:0.4f}")
-                    self.saveGraph(iter)
+                    self.saveGraph(iter, epsd=self.graph_instance_name)
 
                 for row in range(0, len(self.graph[0])):
                     for col in range(0, len(self.graph[row])):
@@ -186,7 +210,10 @@ class GraphGenerator(object):
                             
             t2 = time.time()
             self.runtime = t2 - t1 
-            self.showGraph(iter=iter)
+
+            if verbosity:
+                self.showGraph(iter=iter)
+                self.saveGraph(iter, epsd=self.graph_instance_name)
             logging.info(f"\t{self.graph_instance_name}: {graph_ctf}")    
             self.graph = temp_graph
 
@@ -213,35 +240,64 @@ class GraphGenerator(object):
         return ctf
 
 
-def PlotGraphCTFOverIter(graph_iter_lst, number_iters, verbosity=False):
+
+'''
+Assignment method and graphing methods
+'''
+def toGiff(graph_location):
+    # Create the frames
+    frames = []
+    imgs = glob.glob(f'{graph_location}\\*.png')
+    for i in imgs:
+        new_frame = Image.open(i)
+        frames.append(new_frame)
+    
+    # Save into a GIF file that loops forever
+    frames[0].save(f'{graph_location}.gif', format='GIF',
+                   append_images=frames[1:],
+                   save_all=True,
+                   duration=300, loop=0)
+
+def PlotGraphCTFOverIter(graph_iter_lst, number_iters, t_val, mxd_val, os_val, verbosity=False):
     # print(Graph)
     plt.plot(graph_iter_lst.keys(), graph_iter_lst.values())
-    x_vals_lst = [x_tick_val for x_tick_val in range(0, number_iters)]
+    x_vals_lst = [x_tick_val for x_tick_val in range(0, number_iters, number_iters // 10)]
     y_vals_lst = [y_tick_val for y_tick_val in np.linspace(min(graph_iter_lst.values()), max(graph_iter_lst.values()), 10)]
     plt.xlabel('Iterations')
     plt.ylabel('CTF')
+    plt.legend((f"CTF"), loc='upper center', shadow=True)
     plt.xticks(x_vals_lst)
     plt.yticks(y_vals_lst)
-    plt.title(f'Popullation Schelling Segregation\nAverage CTF Over ({len(graph_iter_lst.keys())}) Iterations')
-    plt.show()
+    plt.title(f'Popullation Schelling Segregation Average\n t:{t_val} mxd:{mxd_val} opn:{os_val} ')
+    plt.savefig(f".\\Content\\GenoratedGraphs\\TestAverage-{t_val}-{mxd_val}-{os_val}-Plot.png")
+
+    if verbosity:
+        plt.show()
+
+    plt.close()
     
-def PlotAvgGraphCTFOverIter(Graph, amount_episodes=3, verbosity=False):
+def PlotAvgGraphCTFOverIter(Graph, training_sesseions=3, verbosity=False):
     graph_agv_acum_dict = dict([(i, j) for i, j in Graph.iter_tracker.items()]) 
-    for episode in range(0, amount_episodes):
+    logging.info(f"Graph -Total {training_sesseions:03}Episode - T-Value -  Open Spots - Split Ratio ")
+    for episode in range(0, training_sesseions):
 
         # Test Schelling Segregation 
-        Graph.setGraphName(f" AvgTestsRun-{episode:03}")
+        logging.info(f"EPSD-{episode:03}-TVAL{Graph.t_value}-OPEN{Graph.open_spots}-MIXD{Graph.red_blue_split:03}")
+        Graph.setGraphName(f"{episode:03}-{Graph.t_value:<0.2}-{Graph.open_spots:<0.2}-{Graph.red_blue_split:03}")
         # Graph.runSchellingSegregationAtRandom(verbosity=verbosity)
         Graph.runSchellingSegregation(verbosity=verbosity)
 
 
         [graph_agv_acum_dict.update({i : j + Graph.iter_tracker.get(i)}) for i, j in graph_agv_acum_dict.items()]
         
-        logging.info(f"\tAverage Run Reults {amount_episodes}:{graph_agv_acum_dict}")
+        logging.info(f"\tAverage Run Reults {training_sesseions}:{graph_agv_acum_dict}")
     
-    [graph_agv_acum_dict.update({i : j / amount_episodes}) for i, j in graph_agv_acum_dict.items()]
+    [graph_agv_acum_dict.update({i : j / training_sesseions}) for i, j in graph_agv_acum_dict.items()]
 
-    PlotGraphCTFOverIter(graph_agv_acum_dict, Graph.num_of_iterations, verbosity=True)
+    PlotGraphCTFOverIter(graph_agv_acum_dict, Graph.num_of_iterations, Graph.t_value, Graph.red_blue_split, Graph.open_spots, verbosity=verbosity)        
+
+
+
 
 
 '''
@@ -256,89 +312,73 @@ TST_GRAPH_RUNS = 3
 logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
 logging.info(f"- - - - - - - - - - - - - -  - - Test Proof Of Concept - - - - - - - - - - - - - - - - -") 
 logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ") 
-tstGraph = GraphGenerator(g_size=5, t=0.1, os=0.2, mxd=0.5, num_iter=15)
-tstGraph.createGraph()
+
 # Test Schelling Segregation 
-tstGraph.runSchellingSegregation(verbosity=True)
+tstGraph = GraphGenerator(g_size=5, t=0.1, os=0.2, mxd=0.5, num_iter=3)
+tstGraph.createGraph()
+tstGraph.runSchellingSegregation(verbosity=False)
 logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
 logging.info(f"- - - - - - - - - - - - - Average Test Run (Limited Verbosity) - - - - - - - - - - - - -") 
 logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ") 
-avgTstGraph = GraphGenerator(g_size=50, t=0.3, os=0.1, mxd=0.3, num_iter=100)
+avgTstGraph = GraphGenerator(g_size=50, t=0.3, os=0.1, mxd=0.4, num_iter=50)
 avgTstGraph.createGraph()
-
-PlotAvgGraphCTFOverIter(avgTstGraph, amount_episodes=1, verbosity=False)
-
-
-
+PlotAvgGraphCTFOverIter(avgTstGraph, training_sesseions=TST_GRAPH_RUNS, verbosity=False)
+print(f"\t\t\t\t\t\t\tTime:{avgTstGraph.runtime}")
+toGiff(f".\\Content\\GenoratedGraphs\\Genorated\\GraphTestSetGraph-000-{avgTstGraph.t_value}-{avgTstGraph.open_spots}-{avgTstGraph.red_blue_split}")
 
 
 
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+logging.info(f"- - - - - - - - - - - - - Variation Test Run (Limited Verbosity) - - - - - - - - - - - -") 
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ") 
+avgTstGraph.setTValue(0.4)
+avgTstGraph.setOSValue(0.1)
+avgTstGraph.setMixedValue(0.4)
+[avgTstGraph.iter_tracker.update({ky:0}) for ky in avgTstGraph.iter_tracker.keys()]
+PlotAvgGraphCTFOverIter(avgTstGraph, training_sesseions=TST_GRAPH_RUNS, verbosity=False)
+print(f"\t\t\t\t\t\t\tTime:{avgTstGraph.runtime}")
+toGiff(f".\\Content\\GenoratedGraphs\\GraphTestSetGraph-000-{avgTstGraph.t_value}-{avgTstGraph.open_spots}-{avgTstGraph.red_blue_split}")
 
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+logging.info(f"- - - - - - - - - - - - - Variation Test Run (Limited Verbosity) - - - - - - - - - - - -") 
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ") 
+avgTstGraph.setTValue(0.5)
+avgTstGraph.setOSValue(0.1)
+avgTstGraph.setMixedValue(0.4)
+[avgTstGraph.iter_tracker.update({ky:0}) for ky in avgTstGraph.iter_tracker.keys()]
+PlotAvgGraphCTFOverIter(avgTstGraph, training_sesseions=TST_GRAPH_RUNS, verbosity=False)
+print(f"\t\t\t\t\t\t\tTime:{avgTstGraph.runtime}")
+toGiff(f".\\Content\\GenoratedGraphs\\GraphTestSetGraph-000-{avgTstGraph.t_value}-{avgTstGraph.open_spots}-{avgTstGraph.red_blue_split}")
 
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+logging.info(f"- - - - - - - - - - - - - Variation Test Run (Limited Verbosity) - - - - - - - - - - - -") 
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ") 
+avgTstGraph.setTValue(0.6)
+avgTstGraph.setOSValue(0.1)
+avgTstGraph.setMixedValue(0.4)
+[avgTstGraph.iter_tracker.update({ky:0}) for ky in avgTstGraph.iter_tracker.keys()]
+PlotAvgGraphCTFOverIter(avgTstGraph, training_sesseions=TST_GRAPH_RUNS, verbosity=False)
+print(f"\t\t\t\t\t\t\tTime:{avgTstGraph.runtime}")
+toGiff(f".\\Content\\GenoratedGraphs\\GraphTestSetGraph-000-{avgTstGraph.t_value}-{avgTstGraph.open_spots}-{avgTstGraph.red_blue_split}")
 
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+logging.info(f"- - - - - - - - - - - - - Variation Test Run (Limited Verbosity) - - - - - - - - - - - -") 
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ") 
+avgTstGraph.setTValue(0.7)
+avgTstGraph.setOSValue(0.1)
+avgTstGraph.setMixedValue(0.4)
+[avgTstGraph.iter_tracker.update({ky:0}) for ky in avgTstGraph.iter_tracker.keys()]
+PlotAvgGraphCTFOverIter(avgTstGraph, training_sesseions=TST_GRAPH_RUNS, verbosity=False)
+print(f"\t\t\t\t\t\t\tTime:{avgTstGraph.runtime}")
+toGiff(f".\\Content\\GenoratedGraphs\\GraphTestSetGraph-000-{avgTstGraph.t_value}-{avgTstGraph.open_spots}-{avgTstGraph.red_blue_split}")
 
-
-
-
-    # def chooseRandomSpot(self):
-    #     while True:
-    #         x =  random.choice([i for i in range(0, int(math.sqrt(self.graph_size)))])
-    #         y =  random.choice([i for i in range(0, int(math.sqrt(self.graph_size)))])
-    #         if self.graph[x][y] != 0:
-    #             break
-    #     return x, y
-    # 
-    # def chooserandomResidence(self):
-    #     psbl_nw_adrs = np.where(self.graph != 0)
-    #     psbl_nw_lst_set = [(psbl_nw_adrs[0][i], psbl_nw_adrs[1][i])for i in range(0, len(psbl_nw_adrs[0]))]
-    #     rndm_res_indx = np.random.choice(range(0, len(psbl_nw_lst_set)))
-    #     x = psbl_nw_lst_set[rndm_res_indx][0]
-    #     y = psbl_nw_lst_set[rndm_res_indx][1]
-    #     return x, y
-
-    # def runSchellingSegregationAtRandom(self, verbosity=False):
-    #     temp_graph = self.graph.copy()   
-    #     t1 = time.time()
-    #     iter = 0
-    #     graph_ctf = 0.0
-
-    #     with alive_bar(self.num_of_iterations, bar='solid', title=f'{self.graph_instance_name}') as bar:
-    #         for iter in range(0, self.num_of_iterations):
-    #             if graph_ctf >= 1:
-    #                 logging.info(f"Homophily! Segregation achived: {graph_ctf}")
-    #                 self.showGraph()
-    #                 break
-    #             if iter % 5 == 0:
-    #                 print(f"\t{self.graph_instance_name}: {graph_ctf}")
-    #                 logging.info(f"\tIteration {iter} : {graph_ctf}")
-
-    #             # random_index_row, random_index_col = self.chooseRandomSpot()
-    #             random_index_row, random_index_col = self.chooserandomResidence()
-    
-    #             if verbosity:
-    #                 logging.info(f" Graph neighbor {team_map.get(self.graph[random_index_row][random_index_col]):>3} @ ({random_index_row}, {random_index_col})")
-
-    #             content_or_not, contentdeness = self.isContent(random_index_row, random_index_col)
-    #             if content_or_not:
-    #                 if verbosity:
-    #                     logging.info(f"\tContent! (%{contentdeness:0.2})")
-    #             else:
-    #                 posible_new_streets = np.where(self.graph==0)
-    #                 random_new_space = random.choice(range(0, len(posible_new_streets[0])))
-    #                 if verbosity:
-    #                     logging.warning("\tDiscontent! (%{contentdeness:0.2})")
-    #                     logging.warning(f"\tPossible Row Address:\t\t{posible_new_streets[0]}")
-    #                     logging.warning(f"\tPossible Column Address:\t{posible_new_streets[1]}")                
-    #                     logging.warning(f"\tCONGRATIONLATIONS {team_map.get(self.graph[random_index_row][random_index_col])}!! YOUR MOVING TO YOUR NEW HOME which is at ({posible_new_streets[0][random_new_space]}, {posible_new_streets[1][random_new_space]}). Moving into {team_map.get(self.graph[posible_new_streets[0][random_new_space]][posible_new_streets[1][random_new_space]])} ")
-                    
-    #                 self.graph[posible_new_streets[0][random_new_space]][posible_new_streets[1][random_new_space]] = self.graph[random_index_row][random_index_col]
-    #                 self.graph[random_index_row][random_index_col] = 0
-    #             graph_ctf =  self.calculateCTF(verbosity=verbosity)
-    #             self.iter_tracker[iter] = graph_ctf
-
-    #             bar()
-    #     t2 = time.time()
-    #     self.runtime = t2 - t1 
-    #     self.showGraph()
-    #     logging.info(f"\t{self.graph_instance_name}: {graph_ctf}")    
-    #     self.graph = temp_graph
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+logging.info(f"- - - - - - - - - - - - - Variation Test Run (Limited Verbosity) - - - - - - - - - - - -") 
+logging.info(f"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ") 
+avgTstGraph.setTValue(0.3)
+avgTstGraph.setOSValue(0.2)
+avgTstGraph.setMixedValue(0.2)
+[avgTstGraph.iter_tracker.update({ky:0}) for ky in avgTstGraph.iter_tracker.keys()]
+PlotAvgGraphCTFOverIter(avgTstGraph, training_sesseions=TST_GRAPH_RUNS, verbosity=False)
+print(f"\t\t\t\t\t\t\tTime:{avgTstGraph.runtime}")
+toGiff(f".\\Content\\GenoratedGraphs\\GraphTestSetGraph-000-{avgTstGraph.t_value}-{avgTstGraph.open_spots}-{avgTstGraph.red_blue_split}")
--- Projects/Project02/Code/ProjectSourceCode/snadbox.py
+++
@@ -1,8 +0,0 @@
-import time 
-
-
-for i in range(0, 10):
-    time.sleep(3)
-    print(i)
-    if i == 2 :
-        break
