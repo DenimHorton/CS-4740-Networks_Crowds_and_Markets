289c1ef9af0f2548402f6726f06cb985f393173f
Robert Denim Horton <Denim_horton@hotmail.com>
2022-03-29 19:46:39 -0600

    Added in simple unit test for now

--- Projects/Project03/Code/Tests/test_graph_build.py
+++ Projects/Project03/Code/Tests/test_graph_build.py
@@ -4,10 +4,31 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from playground import * 
 import unittest
 
-graph1=Graph(1)
+testGraph=Graph("TestGraph")
 
 class TestGraphBuilder(unittest.TestCase):
     
     def test_simple_build(self):
-        print(graph1)
-        self.assertEqual(graph1.graph_name,"Graph--001")
+
+        projectGraphLst = [[0.0, 0.9, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.2, 0.1, 0.4, 0.3, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.4, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.9, 0.0, 0.1, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+                        [0.4, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.4, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.8, 0.0, 0.1, 0.0, 0.0, 0.0, 0.1, 0.0]]
+        projectGraphNP=np.array(projectGraphLst)
+        testGraph.buildGraphFromJSON('.\\Inputs\\GraphTest01.json')
+        comparison = testGraph.network == projectGraphNP
+        self.assertTrue(comparison.all())
+
+    def test_graph_name_setter(self):
+        # Check that graph names gets set correctly
+        self.assertEqual(testGraph.graph_name,"Graph--TestGraph")
+        # Check setter functionality
+        text_graph_name = 'Project Graph Test'
+        testGraph.setGraphName(text_graph_name)
+        self.assertEqual(testGraph.graph_name, f"Graph--{text_graph_name}")
--- Projects/Project03/Code/playground.py
+++ Projects/Project03/Code/playground.py
@@ -1,10 +1,13 @@
 import networkx as nx
+import numpy as np
 import unittest
+import json
 
 class Graph(nx.Graph):
     def __init__(self, grph_nm=""):
         super().__init__()
-        self.graph_name = f"Graph--{grph_nm:03}"
+        self.graph_name = f"Graph--{grph_nm}"
+        self.network = np.array
 
     def __str__(self):
         objStr = f"Grpah with {len(self.nodes)} nodes and {len(self.edges)} edges . . . \n"
@@ -12,6 +15,19 @@ class Graph(nx.Graph):
         for i in self.__dict__:
             objStr += f"\t{i}+\n"        
         return objStr
+    
+    def setGraphName(self, new_grph_nm):
+        self.graph_name = f"Graph--{new_grph_nm}"
+
+    def buildGraphFromJSON(self, json_file_path):
+        jsonFile = open(json_file_path)
+        data = json.load(jsonFile)
+        np_lst_rows = []
+        for tstGraph in data['TestGraphs']:
+            for tstGraphMtrxRow in tstGraph['Matrix']:
+                np_lst_rows.append(tstGraphMtrxRow)
+        self.network = np.array(np_lst_rows)
+        jsonFile.close()
 
 class EndPoint:
     def __init__(self, src_name, dst_name):
@@ -31,6 +47,8 @@ class EndPointWorker:
 
     def start(self):
         self._log('Started thread')
+
+
 
 
 # graph1 = Graph()
